7c4b9d072cc42161eeae50528dba5e2a51fdf8ca
Zero <samuelbustos10903@gmail.com>
2023-05-09 16:52:51 -0300

    logica del ppt añadida

---
+++ piedra-papel-tijeras/src/main.js
@@ -0,0 +1,26 @@
+const botones = document.querySelectorAll('.game__button');
+const piedra = document.querySelector('#piedra');
+const papel = document.querySelector('#papel');
+const tijeras = document.querySelector('#tijeras');
+const ganador = document.querySelector('.game__winner');
+
+botones.forEach((boton) => {
+  boton.addEventListener('click', (event) => {
+    const botChoice = Math.floor(Math.random() * 3) + 1;
+    if (event.target.id === 'piedra') {
+      if (botChoice === 1) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
+      if (botChoice === 2) ganador.innerHTML = 'Has perdido :(';
+      if (botChoice === 3) ganador.innerHTML = 'Has ganado :D';
+    }
+    if (event.target.id === 'papel') {
+      if (botChoice === 1) ganador.innerHTML = 'Has ganado :D';
+      if (botChoice === 2) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
+      if (botChoice === 3) ganador.innerHTML = 'Has perdido :(';
+    }
+    if (event.target.id === 'tijeras') {
+      if (botChoice === 1) ganador.innerHTML = 'Has perdido :(';
+      if (botChoice === 2) ganador.innerHTML = 'Has ganado :D';
+      if (botChoice === 3) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
+    }
+  });
+});
