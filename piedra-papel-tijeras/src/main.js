5394712a75654722c1b79ba0e992f1e71a56c454
Zero <samuelbustos10903@gmail.com>
2023-05-09 17:24:21 -0300

    feat: optimizacion del js y nuevos estilos

--- piedra-papel-tijeras/src/main.js
+++ piedra-papel-tijeras/src/main.js
@@ -1,23 +1,73 @@
 const botones = document.querySelectorAll('.game__button');
 const ganador = document.querySelector('.game__winner');
+const game = document.querySelector('.game');
+
+const reiniciarMensaje = () => {
+  setTimeout(() => {
+    ganador.innerHTML = 'Esperando resultado...';
+    game.style.backgroundColor = 'blueviolet';
+  }, 1500);
+};
+
+const victoria = () => {
+  ganador.innerHTML = 'Has ganado :D';
+  game.style.backgroundColor = '#32CD32';
+};
+const derrota = () => {
+  ganador.innerHTML = 'Has perdido :(';
+  game.style.backgroundColor = '#DC143C';
+};
+const empate = () => {
+  ganador.innerHTML = '¡Empate! Nadie ha ganado.';
+  game.style.backgroundColor = '#D3D3D3';
+};
 
 botones.forEach((boton) => {
   boton.addEventListener('click', (event) => {
     const botChoice = Math.floor(Math.random() * 3) + 1;
+
     if (event.target.id === 'piedra') {
-      if (botChoice === 1) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
-      if (botChoice === 2) ganador.innerHTML = 'Has perdido :(';
-      if (botChoice === 3) ganador.innerHTML = 'Has ganado :D';
+      if (botChoice === 1) {
+        empate();
+        reiniciarMensaje();
+      }
+
+      if (botChoice === 2) {
+        derrota();
+        reiniciarMensaje();
+      }
+      if (botChoice === 3) {
+        victoria();
+        reiniciarMensaje();
+      }
     }
     if (event.target.id === 'papel') {
-      if (botChoice === 1) ganador.innerHTML = 'Has ganado :D';
-      if (botChoice === 2) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
-      if (botChoice === 3) ganador.innerHTML = 'Has perdido :(';
+      if (botChoice === 1) {
+        victoria();
+        reiniciarMensaje();
+      }
+      if (botChoice === 2) {
+        empate();
+        reiniciarMensaje();
+      }
+      if (botChoice === 3) {
+        derrota();
+        reiniciarMensaje();
+      }
     }
     if (event.target.id === 'tijeras') {
-      if (botChoice === 1) ganador.innerHTML = 'Has perdido :(';
-      if (botChoice === 2) ganador.innerHTML = 'Has ganado :D';
-      if (botChoice === 3) ganador.innerHTML = '¡Empate! Nadie ha ganado.';
+      if (botChoice === 1) {
+        derrota();
+        reiniciarMensaje();
+      }
+      if (botChoice === 2) {
+        victoria();
+        reiniciarMensaje();
+      }
+      if (botChoice === 3) {
+        empate();
+        reiniciarMensaje();
+      }
     }
   });
 });
